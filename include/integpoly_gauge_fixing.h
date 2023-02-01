75274d6e21fcf7e373e0d8af69360b1e4a26dc79
jesuel-marques <jesuel.leal@usp.br>
2023-02-01 11:53:01 +0000

    deleted unnecessary files

--- include/integpoly_gauge_fixing.h
+++
@@ -1,9 +0,0 @@
-#ifndef INTEGPOLYGAUGEFIXING_H
-#define INTEGPOLYGAUGEFIXING_H
-
-#include <types.h>
-#include <SU3_ops.h>
-
-int integPolyakovGaugefix(Mtrx3x3 * restrict U, Mtrx3x3 * restrict G);
-
-#endif  //INTEGPOLYGAUGEFIXING_H
